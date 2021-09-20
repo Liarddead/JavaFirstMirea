e18531ec8272662c88670e7afcf3c6c1ebbfe49f
Liarddead <alexe-aksenov@yandex.ru>
2021-09-20 20:38:26 +0300

    Лаб.4-2 Класс Ball составленный по UML-диаграмме Тестовый класс - TestBall

--- src/ru/mirea/task4/task4_2/TestBall.java
+++ src/ru/mirea/task4/task4_2/TestBall.java
@@ -1,6 +1,6 @@
 package ru.mirea.task4.task4_2;
 public class TestBall {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         Ball b1 = new Ball(100, 100);
         System.out.println(b1);
         b1.move(30, 15);
