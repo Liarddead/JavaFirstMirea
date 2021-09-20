426b751be168574de3a691553403cbc3c2c3aa84
Liarddead <alexe-aksenov@yandex.ru>
2021-09-20 18:12:01 +0300

    Лаб.2-3 Класс Book и Тестовый класс

---
+++ src/ru/mirea/task2/task2_3/Book.java
@@ -0,0 +1,42 @@
+package ru.mirea.task2.task2_3;
+public class Book {
+    private String author;
+    private String name;
+    private int year;
+    public  Book(String Author,String Name,int Year){
+        author=Author;
+        name=Name;
+        year=Year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/ru/mirea/task2/task2_3/TestBook.java
@@ -0,0 +1,11 @@
+package ru.mirea.task2.task2_3;
+public class TestBook {
+    public static void main(String[] args) {
+        Book ex1= new Book("J.Rolling","Hary poter",2007);
+        ex1.setAuthor("J.Rowling");
+        ex1.setName("Harry Potter");
+        ex1.setYear(1995);
+        System.out.println(ex1.toString());
+    }
+
+}
