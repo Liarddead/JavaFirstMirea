fb45c17c214827bc789b79088d455df38a91a2f8
Liarddead <alexe-aksenov@yandex.ru>
2021-09-20 16:45:18 +0300

    Лаб.2-1 Класс Shape и Тестовый класс

---
+++ src/ru/mirea/task2/task2_1/TestShape.java
@@ -0,0 +1,9 @@
+package ru.mirea.task2.task2_1;
+public class TestShape {
+    public static void main(String[] args) {
+        Shape ex1=new Shape("Circle","blue");
+        ex1.SetName("square");
+        ex1.SetColour("red");
+        System.out.println(ex1.toString());
+    }
+}
