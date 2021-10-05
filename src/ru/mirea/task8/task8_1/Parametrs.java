fbaccc0392d4a7cc769c3a76da1a35d958189653
Liarddead <alexe-aksenov@yandex.ru>
2021-10-05 21:12:09 +0300

    Лаб.8-1 Класс MainWindow для реализации окна и рисования Класс RandomGeneration для рандомной генерации фигуры и цвета. Тестовый класс - TestWindow

--- src/ru/mirea/task8/task8_1/Parametrs.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.task8.task8_1;
-public interface Parametrs {
-    public int X=0;
-    public   int Y = 0;
-    public int WDT=0;
-    public  int HGT=0;
-    public int number=0;
-}
---
+++ src/ru/mirea/task8/task8_1/TestWindow.java
@@ -0,0 +1,7 @@
+package ru.mirea.task8.task8_1;
+public class TestWindow {
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow();
+        window.paint(null);
+        }
+    }
