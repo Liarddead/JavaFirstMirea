c3bed7ea7f6e21417d03fefb63e24ff044f3b8f0
Liarddead <alexe-aksenov@yandex.ru>
2021-11-14 17:22:06 +0300

    Лаб.11-3 Класс MainWindow реализующий меню с выбором шрифта и цвета текста, Тестовый класс TestWindow.

---
+++ src/ru/mirea/task11/task11_3/TestWindow.java
@@ -0,0 +1,10 @@
+package ru.mirea.task11.task11_3;
+
+import ru.mirea.task11.task11_3.MainWindow;
+
+//
+public class TestWindow {
+    public static void main(String[] args) {
+        MainWindow ex1 = new MainWindow();
+    }
+}
